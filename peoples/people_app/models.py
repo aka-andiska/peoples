6d11831fabaf5325bd8cd2f7bd1b14fb64db2956
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-01-16 16:07:26 +0800

    setting modules.py, setting timezone, and makemigrate

--- peoples/people_app/models.py
+++ peoples/people_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    biography = models.TextField()
+    created_at = models.DateTimeField(
+            default=timezone.now)
+    updated_at = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.updated_at = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
