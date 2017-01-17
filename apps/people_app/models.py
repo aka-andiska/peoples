e4a88b2b1312dbec7bdec73af0ffa8f17b0cb1d2
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-01-17 10:17:41 +0800

    change html file

---
+++ apps/people_app/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class People(models.Model):
+    name = models.CharField(max_length=255)
+    biography = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
