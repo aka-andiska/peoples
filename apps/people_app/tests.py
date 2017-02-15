b8bb8d4bfecd863d4e27f59ef3f9f174c5691ed8
aka-andiska <aka.afenandiska@tampinestechnology.com>
2017-02-15 17:20:00 +0700

    create API Test for Create, Update, and Delete Form on group_list:

--- apps/people_app/tests.py
+++ apps/people_app/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+
 from .models import People, Group
 
 from django.core.urlresolvers import reverse
+
+from rest_framework.test import APIRequestFactory, force_authenticate
+
 
 class PeopleMethodTest(TestCase):
     def test_index(self):
@@ -64,9 +68,10 @@ class PeopleMethodTest(TestCase):
         peoples = People.objects.all()
         self.assertNotIn(to_delete, peoples)
 
-    def test_group(self):
+
+    def test_group_detail(self):
         """
-        group can select data people by group id from table people
+        group can select data people by id from table people
         """
         people_in_group_1 = People.objects.filter(group_id=1)
         people_in_group_2 = People.objects.filter(group_id=2)
@@ -74,42 +79,38 @@ class PeopleMethodTest(TestCase):
         self.assertNotIn(people_in_group_1, people_in_group_2)
 
 
-    def create_Group(self, group_name='group_test', information='testing'):
-        return Group.objects.create(group_name=group_name, information=information)
+    def tes_group_create(self):
+        """
+         Create_Group can create new group
+        """
+        test = Group.objects.create(name="test", information="testing")
 
-    # def test_create_group(self, group_instance=None):
-    #     """
-    #     group can create new group
-    #     """
-    #     lion = Group.objects.create(name="lion", information="roar")
-    #
-    #
-    #     response = self.client.post(reverse('group_list')) #this is how to act
-    #
-    #     self.assertEqual(response.status_code, 200) #this is how to assert
-    #     self.assertEqual(response.context['group'][0], lion)
+        response = self.client.post(reverse('group_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['group'][0], test)
 
 
-    # def test_group_edit(self):
-    #     """
-    #     group can get data by id to editing
-    #     """
-    #     pass
-    #
-    # def test_group_update(self):
-    #     """
-    #     group can update data
-    #     """
-    #     pass
-    #
-    # def test_group_delet(self):
-    #     """
-    #     group can delete data by id
-    #     """
-    #     people = Group.objects.all()
-    #     to_delete = Group.objects.filter(id=89)
-    #
-    #     self.assertNotIn(people, to_delete)
+    def test_group_update(self):
+        """
+        update can be post data group to group list after editing
+        """
+        group1 = Group.objects.all()
+        to_update = Group.objects.filter(id=1)
+
+        response = self.client.post(reverse('group_list'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotIn(to_update, group1)
+
+
+    def test_group_delete(self):
+        """
+        delete can deleting group by id
+        """
+        to_delete = Group.objects.filter(id=2)
+        groups = Group.objects.all()
+        self.assertNotIn(to_delete, groups)
 
 
 
